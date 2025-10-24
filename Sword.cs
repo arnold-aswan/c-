8754affd71445b26a3098a2b79f4f4fec2e4ae84
arnold-aswan <fredyaswan@gmail.com>
2025-10-24 13:50:09 +0300

    finished weapon forge exercise

--- Sword.cs
+++ Sword.cs
@@ -3,7 +3,6 @@ namespace WeaponForge;
 class Sword : Weapon
 {
     public Sword(string name, int damage) : base(name, damage){ }
-
     public override void DisplayInfo() => Console.WriteLine($"{Name}: slashing weapon type Sword, damage: {Damage} ");
     public override void Use() => Console.WriteLine("Swish");
 }
@@ -11,7 +10,6 @@ class Sword : Weapon
 class Bow : Weapon
 {
     public Bow(string name, int damage) : base(name, damage){ }
-
     public override void DisplayInfo() => Console.WriteLine($"{Name}: weapon type Bow, used with arrows. damage: {Damage} ");
     public override void Use() => Console.WriteLine("Wuush");
 }
@@ -19,7 +17,6 @@ class Bow : Weapon
 class Staff : Weapon
 {
     public Staff(string name, int damage) : base(name, damage){ }
-
     public override void DisplayInfo() => Console.WriteLine($"{Name} weapon type Staff, damage: {Damage} ");
     public override void Use() => Console.WriteLine("smack");
 }
